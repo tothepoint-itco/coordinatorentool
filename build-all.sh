d455bbd98c45e161a6b2ff83b6ad2205c9f8fd29
Kaj Van der Hallen <kaj.vanderhallen@foresee.be>
2016-04-12 12:06:54 +0200

    Removed wrong "cd .." command from build-all.sh
    Added bediende-service to the build-all.sh and the docker compose file

--- build-all.sh
+++ build-all.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-cd ..
-
 echo Building discovery-server...
 cd coordinatoren-discovery; gradle --daemon clean build distDocker; cd -
 
 echo Building business-unit-service...
 cd coordinatoren-businessunit-service; gradle --daemon clean build distDocker; cd -
+
+echo Building bediende-service...
+cd coordinatoren-bediende-service; gradle --daemon clean build distDocker; cd -
 
 echo Building contract-service...
 cd coordinatoren-contract-service; gradle --daemon clean build distDocker; cd -
