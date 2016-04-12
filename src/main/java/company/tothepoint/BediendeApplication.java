e79050a3e4bff2092328f60d8e7d1fe65b665d78
Kaj Van der Hallen <kaj.vanderhallen@foresee.be>
2016-04-12 17:20:05 +0200

    Registered service using EurekaClient

--- src/main/java/company/tothepoint/BediendeApplication.java
+++ src/main/java/company/tothepoint/BediendeApplication.java
@@ -7,9 +7,11 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BediendeApplication {
 	private static final String BEDIENDE_EXCHANGE = "bediende-exchange";
 	private static final String BEDIENDE_ROUTING = "bediende-routing";
