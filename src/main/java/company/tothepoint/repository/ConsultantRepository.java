63afc2612e1b8c304f629efe6783b1ce6c13afe5
butrint <butrint.zymberi@hotmail.be>
2016-04-22 18:13:08 +0200

    Added beheer service

--- src/main/java/company/tothepoint/repository/ConsultantRepository.java
+++
@@ -1,7 +0,0 @@
-package company.tothepoint.repository;
-
-import company.tothepoint.model.consultant.Consultant;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ConsultantRepository extends MongoRepository<Consultant, String> {
-}
