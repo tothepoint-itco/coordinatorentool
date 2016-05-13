07a742010638c8ae300be58dd70601b7be296737
Kaj Van der Hallen <kaj.vanderhallen@foresee.be>
2016-05-13 23:37:58 +0200

    Updated git submodules

--- build-all.sh
+++ build-all.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 
 echo Building discovery-server...
-cd coordinatoren-discovery; gradle --daemon clean build distDocker; cd -
+cd coordinatoren-discovery; gradle --daemon clean build distDocker -x test; cd -
 
 echo Building edge-server...
-cd coordinatoren-edge; gradle --daemon clean build distDocker; cd -
+cd coordinatoren-edge; gradle --daemon clean build distDocker -x test; cd -
 
 echo Building business-unit-service...
-cd coordinatoren-businessunit-service; gradle --daemon clean build distDocker; cd -
+cd coordinatoren-businessunit-service; gradle --daemon clean build distDocker -x test -x asciidoctor; cd -
 
-echo Building bediende-service...
-cd coordinatoren-bediende-service; gradle --daemon clean build distDocker; cd -
+#echo Building bediende-service...
+#cd coordinatoren-bediende-service; gradle --daemon clean build distDocker -x test -x asciidoctor; cd -
 
 echo Building contract-service...
-cd coordinatoren-contract-service; gradle --daemon clean build distDocker; cd -
+cd coordinatoren-contract-service; gradle --daemon clean build distDocker -x test -x asciidoctor; cd -
 
 echo Building planning-service...
-cd coordinatoren-planning-service; gradle --daemon clean build distDocker; cd -
+cd coordinatoren-planning-service; gradle --daemon clean build distDocker -x test -x asciidoctor; cd -
+
+#echo Building beheer-service...
+#cd coordinatoren-beheer-service; gradle --daemon clean build distDocker -x test -x asciidoctor; cd -
